ea9440fc7efae1b9e031118c51fa3eb2686a6375
Troy Kollar <troykollar@gmail.com>
2022-11-10 18:22:51 -0500

    flesh out game store

--- state/game.ts
+++ state/game.ts
@@ -1,17 +1,27 @@
-import create from "zustand"
+import create from "zustand";
 
 const maxLetters = 5;
 
 interface GameState {
-    currentGuess: string,
-    addToCurrentGuess: (letter: string) => void,
-    deleteFromCurrentGuess: () => void,
+  currentGuess: string;
+  addToCurrentGuess: (letter: string) => void;
+  deleteFromCurrentGuess: () => void;
 }
 
 const useGameStore = create<GameState>((set) => ({
-    currentGuess: "",
-    addToCurrentGuess: (letter) => {
-        if ()
-    },
-    deleteFromCurrentGuess: () => {}
-}))
+  currentGuess: "",
+  addToCurrentGuess: (letter: string) =>
+    set((state) => {
+      let guess = state.currentGuess;
+      if (state.currentGuess.length < maxLetters) guess += letter;
+      return { ...state, currentGuess: guess };
+    }),
+  deleteFromCurrentGuess: () =>
+    set((state) => {
+      let guess = state.currentGuess;
+      if (guess.length > 0) guess = guess.slice(0, -1);
+      return { ...state, currentGuess: guess };
+    }),
+}));
+
+export default useGameStore;
