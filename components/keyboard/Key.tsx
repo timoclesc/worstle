6e2087dcf429c22a269dc317a3dfe71682d97f82
Troy Kollar <troykollar@gmail.com>
2022-11-20 00:20:46 -0500

    Fix keyboard colors

--- components/keyboard/Key.tsx
+++ components/keyboard/Key.tsx
@@ -15,13 +15,25 @@ const Key: FunctionComponent<KeyProps> = ({
   evaluation,
 }) => {
   const width = large ? "w-16" : "w-11";
-  let bgColor = "bg-gray-500";
-  if (evaluation) bgColor = EvaluationColor[evaluation];
+  let bgColor = "bg-light-gray dark:bg-[#818384]";
+  switch (evaluation) {
+    case "absent":
+      bgColor = "bg-light-eval-absent dark:bg-dark-eval-absent";
+      break;
+    case "present":
+      bgColor = "bg-light-eval-present dark:bg-dark-eval-present";
+      break;
+    case "correct":
+      bgColor = "bg-light-eval-correct dark:bg-dark-eval-correct";
+      break;
+  }
+
+  let textColor = evaluation ? "text-white" : "text-black dark:text-white";
 
   return (
     <button
       onClick={onClick}
-      className={`${width} ${bgColor} text-white rounded font-mono uppercase h-14 flex justify-center items-center transition-colors duration-200 delay-[1500ms]`}
+      className={`${width} ${bgColor} ${textColor} rounded font-mono uppercase h-14 flex justify-center items-center transition-colors duration-200 delay-[1500ms]`}
     >
       {children}
     </button>
